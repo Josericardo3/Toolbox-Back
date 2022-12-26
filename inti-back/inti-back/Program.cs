427d42d3dcacc5ea2150f1dce09b33af4dfab7b9
Rodrigo Gutierrez <rgutierrez1@rbi.com>
2022-12-26 14:59:21 -0500

    added sql connector logic

--- inti-back/inti-back/Program.cs
+++ inti-back/inti-back/Program.cs
@@ -2,19 +2,34 @@ using inti_repository;
 using inti_model;
 using MySql.Data.MySqlClient;
 
+const String default_url = "http://{0}:{1}";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+var port = Environment.GetEnvironmentVariable("PORT");
+var host = Environment.GetEnvironmentVariable("HOST");
+var env = Environment.GetEnvironmentVariable("ENV");
+
+String connectionString = env != "DEV" ? "MySqlConnectionDev" : "MySqlConnection";
+
+
+Console.WriteLine("Connection string {0}", connectionString);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.WebHost.UseUrls("http://0.0.0.0:8050");
+String to_use_urls = String.Format(default_url, host, port);
+
+Console.WriteLine(to_use_urls);
+
+builder.WebHost.UseUrls(to_use_urls);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-var MySqlConfiguration = new MySQLConfiguration(builder.Configuration.GetConnectionString("MySqlConnection"));
+var MySqlConfiguration = new MySQLConfiguration(builder.Configuration.GetConnectionString(connectionString));
 builder.Services.AddSingleton(MySqlConfiguration);
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
