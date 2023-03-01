fa798c9440d8569156b695db415ccda60dee2801
Aaron Zevallos <practicas.0070@SESITDIGITAL.com>
2023-03-01 11:12:07 -0500

    encriptacion

--- inti-back/inti-back/Controllers/UsuarioController.cs
+++ inti-back/inti-back/Controllers/UsuarioController.cs
@@ -592,26 +592,6 @@ namespace inti_back.Controllers
                 client.Disconnect(true);
             }
 
-        [HttpGet("ListaChequeo")]
-        public async Task<IActionResult> GetResponseArchivoListaChequeo(int idnorma, int idusuariopst)
-        {
-            string ValorMaestroValorTituloListaChequeo = this.Configuration.GetValue<string>("ValorMaestro:ValorTituloListaChequeo");
-            string ValorMaestroValorListaChequeo = this.Configuration.GetValue<string>("ValorMaestro:ValorSeccionListaChequeo");
-            string ValorMaestroValordescripcionCalificacion = this.Configuration.GetValue<string>("ValorMaestro:ValordescripcionCalificacion");
-
-            try
-            {
-                var response = await _usuarioPstRepository.GetResponseArchivoListaChequeo(idnorma, idusuariopst,Convert.ToInt32(ValorMaestroValorTituloListaChequeo), Convert.ToInt32(ValorMaestroValorListaChequeo), Convert.ToInt32(ValorMaestroValordescripcionCalificacion));
-                return Ok(response);
-            }
-            catch (Exception)
-            {
-                return Ok(new
-                {
-                    StatusCode(200).StatusCode,
-                    valor = "Error al momento de obtener el archivo"
-                });
-            }
         }
 
 
