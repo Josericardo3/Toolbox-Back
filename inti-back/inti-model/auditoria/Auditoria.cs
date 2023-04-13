23cee6cc82e4ddda5cfd9ff8e4b3e51bd4296d2d
Felix V <felix.vicente@sesitdigital.com>
2023-04-13 12:21:31 -0500

    update insert auditoria

--- inti-back/inti-model/auditoria/Auditoria.cs
+++ inti-back/inti-model/auditoria/Auditoria.cs
@@ -10,6 +10,7 @@ namespace inti_model.auditoria
     public class Auditoria
     {
         public int ID_AUDITORIA { get; set; }
+        public int FK_ID_PST { get; set; }
         public string? CODIGO { get; set; }
         public DateTime FECHA_AUDITORIA { get; set; }
         public string? PROCESO { get; set; }
